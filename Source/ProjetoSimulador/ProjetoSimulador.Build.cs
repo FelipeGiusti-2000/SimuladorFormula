2cb718f41b9b8f5add5fe59aa360813f4f0cfe68
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2020-10-14 16:18:07 -0300

    fim da implementacao do main menu, mudanca nos levels
    fim do main menu inicial, criacao de levels para cada um ter seu gamemode

--- Source/ProjetoSimulador/ProjetoSimulador.Build.cs
+++ Source/ProjetoSimulador/ProjetoSimulador.Build.cs
@@ -8,9 +8,11 @@ public class ProjetoSimulador : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay","SlateCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
